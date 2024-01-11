4d49e351001acb87655aa51a19ae7b51787f1eb0
TadaRyoto <154503429+TadaRyoto@users.noreply.github.com>
2024-01-11 18:36:55 +0900

    「解答を表示」で解説も表示されるよう変更

--- static/exercise.js
+++ static/exercise.js
@@ -1,7 +1,9 @@
 const showAnswerButton = document.getElementById("show-answer-button");
 const answer = document.getElementById("answer");
+const explanation = document.getElementById("explanation");
 
 showAnswerButton.onclick = () => {
   answer.style.display = "block";
+  explanation.style.display = "block";
   showAnswerButton.style.display = "none";
 };
